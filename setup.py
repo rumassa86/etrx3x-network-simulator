0ff93cc1c878dbd0bed78fbec04445d9c1974134
Rubens Suguimoto <rubens@smartgreen.net>
2017-06-22 14:37:38 -0300

    FIX setup.py: correções de sintaxes

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 setup(
     name='etrx3x_sim',
     version='0.0.0',
-    description='ETRX3x simulator to test performance of SG Gateway (Concentrador)',
-    url='http://repositorio.smartgreen.net.br/network/etrx3x-network-simulator',
+    description='ETRX3x simulator to test performance of SG Gateway'
+    ' (Concentrador)',
+    url='http://repositorio.smartgreen.net.br/network/etrx3x-network-'
+    'simulator',
     author='Rubens Massayuki Suguimoto',
     author_email='rubens@smartgreen.com',
     license='SG',
